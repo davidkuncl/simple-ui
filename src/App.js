cc46e7c6700999ae7bff75eeef7eda4479b5cc05
Dave Kuncl <david.kuncl@battlespace.us>
2020-08-12 16:39:56 -0700

    more stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import AppBar from '@material-ui/core/AppBar';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Toolbar from '@material-ui/core/Toolbar';
+// import Nav from './components/Nav'
+
 function App() {
+  const [data, setData] = useState({ drinks: [] });
+
+  useEffect(() => {
+    // const apiUrl = 'https://hn.algolia.com/api/v1/search?query=redux';
+    const apiUrl = 'http://localhost:3000/test';
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => setData(data));
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+        <Toolbar>
+          React Material UI Example
+        </Toolbar>
+      </AppBar>
+      <br/><br/>
+      <Paper elevation={3}>
+        <Table size="small" >
+          <TableHead>
+            <TableRow>
+              <TableCell>Date</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">Sale Amount</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.drinks.map(item => (
+              <TableRow key={item.name}>
+                <TableCell>{item.name}</TableCell>
+                <TableCell>{item.type}</TableCell>
+                <TableCell align="right">{item.cals}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+// Nav.js
+
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+const Nav = () => {
+    return(
+        <div>
+        <AppBar position="static">
+            <Toolbar>
+                React Material UI Example
+            </Toolbar>
+        </AppBar>
+        </div>
+    )
+}
+
+export default Nav;
